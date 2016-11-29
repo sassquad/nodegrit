30e7fa5c5d746198db854b0b274551ba47e7c627
Stephen Scott <sa_scott@yahoo.com>
2016-11-29 10:29:59 +0000

    More fancy output
    Use of chalk and figlet modules for nicer output. Added random npm module to randomise output.

--- 2016sim/app.js
+++ 2016sim/app.js
@@ -1,15 +1,30 @@
+#!/usr/bin/env node
+
 "use strict";
 
-console.log('Starting 2016 Simulator\n');
-
+var chalk = require('chalk');
+var figlet = require('figlet');
+var randomInt = require('random-int');
 var ohdear = require('./ohdear.js');
 
-var output = ohdear.something();
+// Clear screen
+process.stdout.write('\x1Bc');
 
-console.log("####################");
-console.log("#  2016 SIMULATOR  #");
-console.log("####################");
+// Display title 
+console.log(
+  chalk.yellow(
+    figlet.textSync('2016 Simulator', { horizontalLayout: 'full' }, function(err, data) {
+    	if (err) {
+    		console.log('Something went wrong...');
+    		console.dir(err);
+    		return;
+    	}
+    	console.log(data);
+    })
+  )
+);
 
+// Start simulation
 setInterval(function(){
-  console.log(output);
+  console.log(chalk.red(ohdear.something(randomInt(5))));
 }, 2000);      
--- 2016sim/ohdear.js
+++ 2016sim/ohdear.js
@@ -1,7 +1,12 @@
 "use strict";
 
-console.log('Starting ohdear.js');
-
-module.exports.something = () => {
-	return 'Something shit has happened\n';
+module.exports.something = (random) => {
+	switch (random) {
+		case 0: return 'Adam Sandler is still making movies\n';
+		case 1: return 'Something shit has happened\n';
+		case 2: return 'President Elect Trump tweeted something\n';
+		case 3: return 'David Bowie is now a REAL Starman\n';
+		case 4: return 'A politician has said the word Brexit\n';
+		case 5: return 'UKIP still exists\n';
+	}
 };
